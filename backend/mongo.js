020cf06aa09d70d711f985342275d7e7f33cb428
abdihasib <abdihasib.isse@outlook.com>
2023-06-22 21:01:38 +1000

    update db url

--- backend/mongo.js
+++ backend/mongo.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
+
 if (process.argv.length<3) {
   console.log('give password as argument')
   process.exit(1)
@@ -8,7 +9,7 @@ if (process.argv.length<3) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
+  `mongodb+srv://fullstack:${password}@cluster0.iwnyroa.mongodb.net/noteApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
