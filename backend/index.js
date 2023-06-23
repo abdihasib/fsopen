1ce27ec88eec50564001a28a58429f88a41696f5
abdi-edstart <abdihasib.isse@edstart.com.au>
2023-06-23 16:57:55 +1000

    add mongon backend

--- backend/index.js
+++ backend/index.js
@@ -1,84 +1,101 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Note = require('./models/note')
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-01-10T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-01-10T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-01-10T19:20:14.298Z",
-    important: true
-  }
-]
+const PORT = process.env.PORT
 
 app.use(express.json())
+
+
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-} 
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
-
-  const note = {
+  const newNote = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  newNote.save().then(result => {
+    console.log('note saved!')
+    response.json(result)
+  }).catch(err => next(err))
 })
+
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes)
+  Note.find({}).then(result => {
+    console.log(result)
+    res.json(result)
+  })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
 
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
 
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id).then(note => {
+    if (note) {
+      response.json(note)
+    }
+    else {
+      response.status(404).end()
+    }
+  }).catch(err => next(err))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+
+  const { content, important } = request.body
+
+  Note.findByIdAndUpdate(
+    request.params.id, 
+
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  ) 
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
-})
 
-const PORT = 3001
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- backend/mongo.js
+++ backend/mongo.js
@@ -21,12 +21,39 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-  content: 'HTML is Easy',
-  important: true,
-})
+// const note = new Note({
+//   content: 'HTML is Easy',
+//   important: true,
+// })
 
-note.save().then(result => {
-  console.log('note saved!')
+// const newNote = new Note({
+//   content: 'javascript is confusing',
+//   important: false,
+// })
+
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
+
+// newNote.save().then(result => {
+//   console.log('new note saved!')
+//   mongoose.connection.close()
+// })
+
+// empty object in parameter is search conditions. empty object means we get all the notes
+// search conditions adhere to the mongo search query syntax
+// Note.find({}).then(result => {
+//   result.forEach(note => {
+//     console.log(note)
+//   })
+//   mongoose.connection.close()
+// })
+
+// get only important notes:
+Note.find({important: true}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
   mongoose.connection.close()
 })
